19df63dcae9a3b475dd2e12d8570119ad33d62a2
adhithiyan <adhithiyan.work@gmail.com>
2023-11-22 02:44:05 +0530

    add: package files

---
+++ src/script.js
@@ -0,0 +1,34 @@
+function dropDown(parentDiv, menuValue, dropDownObject) {
+  /* a parent div with a button and a list as childs */
+  let dropDownDiv = createDropDown(menuValue, dropDownObject);
+  parentDiv.appendChild(dropDownDiv);
+}
+
+function createDropDown(btnValue, dropDownObject) {
+  const dropDownDiv = document.createElement('div');
+  dropDownDiv.classList.add('dropDown');
+
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.textContent = btnValue;
+
+  let listDiv = document.createElement('ul');
+
+  btn.addEventListener('click', () => {
+    listDiv.classList.toggle('toggle');
+  });
+
+  dropDownDiv.appendChild(btn);
+  for (let i in dropDownObject) {
+    let anchor = document.createElement('a');
+    anchor.textContent = i;
+    anchor.setAttribute('href', dropDownObject[i]);
+    listDiv.appendChild(anchor);
+  }
+  dropDownDiv.appendChild(listDiv);
+
+  return dropDownDiv;
+}
+
+let body = document.querySelector('body');
+dropDown(body, 'btn', { a: '#', b: '#' });
